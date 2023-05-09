c503c28eca339f0a9ea20cb116c49253e2f09e25
Aukusti Palomäki <Daupa001@edu.xamk.fi>
2023-05-09 15:04:41 +0300

    + form tag

--- src/components/screens/survey-listing-screen.tsx
+++ src/components/screens/survey-listing-screen.tsx
@@ -84,7 +84,7 @@ const SurveyListingScreen: React.FC = () => {
         { LocalizationUtils.getLocalizedUnits(unit) }
       </MenuItem>
     );
-
+    
   /**
   * Render listing UI
   */
@@ -102,200 +102,210 @@ const SurveyListingScreen: React.FC = () => {
           <Typography variant="h1" margin={1} textAlign="center">
             Tee ilmoitus kauppapaikkaan
           </Typography>
-          <TextField
-            fullWidth
-            color="primary"
-            name="componentName"
-            label="Ilmoituksen otsikko"
-            value="ovi"
-          />
+          <form onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              color="primary"
+              name="componentName"
+              label="Ilmoituksen otsikko"
+              value="ovi"
+              disabled
+            />
         
-          <Stack
-            direction="row"
-            spacing={ 2 }
-            marginTop={ 2 }
-          >
-            <TextField
-              fullWidth
-              select
-              color="primary"
-              name="reusableMaterialId"
-              value="rakennusosat"
-              label="Materiaali"
-              helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartOrMaterialHelperText }
-            />
-            <TextField
-              fullWidth
-              select={false}
-              color="primary"
-              name="reusableMaterialId"
-              value="valittu rakennusosa"
-              label="Materiaali"
-            />
-          </Stack>
-          <TextField
-            multiline
-            rows={ 2 }
-            name="description"
-            label="Materiaalin kuvaus"
-            value={newMaterial.description}
-            helperText={ strings.survey.reusables.addNewBuildingPartsDialog.descriptionHelperText }
-            onChange={ onNewMaterialChange }
-          />
-          <Stack
-            direction="row"
-            spacing={ 2 }
-            marginTop={ 2 }
-          >
-            { /* Määrät */ }
-            <TextField
-              fullWidth
-              color="primary"
-              name="amount"
-              value={ newMaterial.amount }
-              label="Arvio materiaalin määrästä"
-              type="number"
-              onChange={ onNewMaterialChange }
-            />
-            <TextField
-              fullWidth
-              select={false}
-              color="primary"
-              name="amount"
-              value={ newMaterial.amount }
-              label="Annettu arvo"
-              type="number"
-              onChange={ onNewMaterialChange }
-            />
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={ 2 }
-            marginTop={ 2 }
-          >
-            <TextField
-              fullWidth
-              select
-              name="unit"
-              color="primary"
-              value={ newMaterial.unit }
-              label={ strings.survey.reusables.dataGridColumns.unit }
-              onChange={ onNewMaterialChange }
+            <Stack
+              direction="row"
+              spacing={ 2 }
+              marginTop={ 2 }
             >
-              { unitOptions }
-            </TextField>
-            <TextField
-              fullWidth
-              select={false}
-              color="primary"
-              name="amount"
-              value={ newMaterial.amount }
-              label="Annettu arvo"
-              type="number"
-              onChange={ onNewMaterialChange }
-            />
-          </Stack>
-          <Stack spacing={ 2 } marginTop={ 2 }>
+              <TextField
+                fullWidth
+                select
+                color="primary"
+                name="reusableMaterialId"
+                value="rakennusosat"
+                label="Materiaali"
+                helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartOrMaterialHelperText }
+              />
+              <TextField
+                fullWidth
+                select={false}
+                color="primary"
+                name="reusableMaterialId"
+                value="valittu rakennusosa"
+                label="Materiaali"
+                disabled
+              />
+            </Stack>
             <TextField
               multiline
               rows={ 2 }
               name="description"
-              label="Lisätieto määrästä"
-              value={ newMaterial.description }
+              label="Materiaalin kuvaus"
+              value={newMaterial.description}
               helperText={ strings.survey.reusables.addNewBuildingPartsDialog.descriptionHelperText }
               onChange={ onNewMaterialChange }
             />
-          </Stack>
-          { /* Location of the material */ }
-          <Stack spacing={ 2 } marginTop={ 2 }>
+            <Stack
+              direction="row"
+              spacing={ 2 }
+              marginTop={ 2 }
+            >
+              { /* Määrät */ }
+              <TextField
+                fullWidth
+                color="primary"
+                name="amount"
+                value={ newMaterial.amount }
+                label="Arvio materiaalin määrästä"
+                type="number"
+                onChange={ onNewMaterialChange }
+              />
+              <TextField
+                fullWidth
+                select={false}
+                color="primary"
+                name="amount"
+                value={ newMaterial.amount }
+                label="Annettu arvo"
+                type="tel"
+                onChange={ onNewMaterialChange }
+                disabled
+              />
+            </Stack>
+            <Stack
+              direction="row"
+              spacing={ 2 }
+              marginTop={ 2 }
+            >
+              <TextField
+                fullWidth
+                select
+                name="unit"
+                color="primary"
+                value={ newMaterial.unit }
+                label={ strings.survey.reusables.dataGridColumns.unit }
+                onChange={ onNewMaterialChange }
+              >
+                { unitOptions }
+              </TextField>
+              <TextField
+                fullWidth
+                select={false}
+                color="primary"
+                name="amount"
+                value={ newMaterial.amount }
+                label="Annettu arvo"
+                type="tel"
+                onChange={ onNewMaterialChange }
+                disabled
+              />
+            </Stack>
+            <Stack spacing={ 2 } marginTop={ 2 }>
+              <TextField
+                multiline
+                rows={ 2 }
+                name="description"
+                label="Lisätieto määrästä"
+                value={ newMaterial.description }
+                helperText={ strings.survey.reusables.addNewBuildingPartsDialog.descriptionHelperText }
+                onChange={ onNewMaterialChange }
+              />
+            </Stack>
+            { /* Location of the material */ }
+            <Stack spacing={ 2 } marginTop={ 2 }>
+              <TextField
+                fullWidth
+                color="primary"
+                name="componentName"
+                label="Kohteen nimi"
+                value={ newMaterial.componentName }
+                helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
+                onChange={ onNewMaterialChange }
+              />
+            </Stack>
+            <Stack
+              direction="row"
+              spacing={ 2 }
+              marginTop={ 2 }
+            >
+              <TextField
+                fullWidth
+                color="primary"
+                name="componentName"
+                label="Katuosoite"
+                value={ newMaterial.componentName }
+                helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
+                onChange={ onNewMaterialChange }
+              />
+              <TextField
+                fullWidth
+                color="primary"
+                name="componentName"
+                label="Annettu arvo"
+                type="text"
+                value={ newMaterial.componentName }
+                onChange={ onNewMaterialChange }
+                disabled
+              />
+            </Stack>
+            <Stack
+              direction="row"
+              spacing={ 2 }
+              marginTop={ 2 }
+            >
+              <TextField
+                fullWidth
+                color="primary"
+                name="componentName"
+                label="Postinumero"
+                value={ newMaterial.componentName }
+                helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
+                onChange={ onNewMaterialChange }
+              />
+              <TextField
+                fullWidth
+                color="primary"
+                name="componentName"
+                label="Annettu arvo"
+                type="text"
+                value={ newMaterial.componentName }
+                onChange={ onNewMaterialChange }
+                disabled
+              />
+            </Stack>
+            {/* Yhteystiedot */}
             <TextField
               fullWidth
               color="primary"
               name="componentName"
-              label="Kohteen nimi"
+              label="Nimi"
               value={ newMaterial.componentName }
               helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
               onChange={ onNewMaterialChange }
             />
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={ 2 }
-            marginTop={ 2 }
-          >
+            { /* puh */ }
             <TextField
               fullWidth
               color="primary"
               name="componentName"
-              label="Katuosoite"
-              value={ newMaterial.componentName }
-              helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
+              label="Puhelinnumero"
+              type="tel"
+              value={ newMaterial.unit }
+              helperText="040 ..."
               onChange={ onNewMaterialChange }
             />
+            { /* e-mail */ }
             <TextField
               fullWidth
               color="primary"
               name="componentName"
-              label="Annettu arvo"
+              label="Sähköposti"
+              type="email"
               value={ newMaterial.componentName }
               helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
               onChange={ onNewMaterialChange }
             />
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={ 2 }
-            marginTop={ 2 }
-          >
-            <TextField
-              fullWidth
-              color="primary"
-              name="componentName"
-              label="Postinumero"
-              value={ newMaterial.componentName }
-              helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
-              onChange={ onNewMaterialChange }
-            />
-            <TextField
-              fullWidth
-              color="primary"
-              name="componentName"
-              label="Annettu arvo"
-              value={ newMaterial.componentName }
-              helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
-              onChange={ onNewMaterialChange }
-            />
-          </Stack>
-          {/* Yhteystiedot */}
-          <TextField
-            fullWidth
-            color="primary"
-            name="componentName"
-            label="Nimi"
-            value={ newMaterial.componentName }
-            helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
-            onChange={ onNewMaterialChange }
-          />
-          { /* puh */ }
-          <TextField
-            fullWidth
-            color="primary"
-            name="componentName"
-            label="Puhelinnumero"
-            value={ newMaterial.unit }
-            helperText="040 ..."
-            onChange={ onNewMaterialChange }
-          />
-          { /* e-mail */ }
-          <TextField
-            fullWidth
-            color="primary"
-            name="componentName"
-            label="Sähköposti"
-            value={ newMaterial.componentName }
-            helperText={ strings.survey.reusables.addNewBuildingPartsDialog.buildingPartHelperText }
-            onChange={ onNewMaterialChange }
-          />
+          </form>
           <Stack
             direction="row"
             spacing={ 2 }
